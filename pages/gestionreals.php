95490e534e0d5c2b8e23f6e97ace7ddbbd669d48
Benoit Groneau <groneau@info03-15.iut45.univ-orleans.fr>
2020-10-17 09:16:23 +0200

    add gestion

---
+++ pages/gestionreals.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Autoloader;
+use App\Tables\Realisateur;
+
+require_once '../src/Autoloader.php';
+Autoloader::register();
+
+$reals = new Realisateur();
+$listereals = $reals->findAll();
+
+$title = 'Gestion des réalisateurs';
+?>
+
+<div class="container">
+    <h1>GESTION REALS</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Prenom</th>
+                <th scope="col">Année de naissance</th>
+                <th scope="col">Biographie</th>
+                <th scope="col">Edit</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($listereals as $real): ?>
+            <tr>
+                <th scope="row"><?= $real->id ?></th>
+                <td><?= $real->nomreal ?></td>
+                <td><?= $real->prenomreal ?></td>
+                <td><?= $real->naissancereal ?></td>
+                <td>
+                    <?php
+                    $string = $real->biographie;
+                    $max = 20; // or 200, or whatever
+                    if(strlen($string) > $max) {
+                        // find the last space < $max:
+                        $shorter = substr($string, 0, $max+1);
+                        $string = substr($string, 0, strrpos($shorter, ' ')).' ...';
+                    }
+                    echo $string;
+                    ?>
+                </td>
+                <td>
+                    <form action="/public/index.php?p=deletereals&id=<?= $real->id ?>" method="post" onsubmit="return confirm('Voulez vraiment effectuer cette action?')">
+                        <button type="submit" class="btn btn-danger">Supprimer</button>
+                    </form>
+                </td>
+            </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+</div>
